d9c4352f5c6db784fbe7df7c11f05f216788f6d6
coolnayak <coolnayak@7f586056-a187-11dd-932a-8f5888711c47>
2009-06-16 06:52:04 +0000

    Created the branch for command_line_parsing project.

---
+++ trunk/projects/command_line_parsing/src/commandlineparser_inl.h
@@ -0,0 +1,39 @@
+/*
+* Copyright (C) 2009 Raghavendra Nayak
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// this file contains all the inline implementation of class CommandLineParser.
+
+#ifndef _COMMANDLINEPARSER_INL_H_
+#define _COMMANDLINEPARSER_INL_H_
+
+#include "config.h"
+
+UseCliNamespace
+
+//////////////////////////////////////////////////////////////////////////////////
+//
+///// @brief method to get the current style.
+///// @param none.
+///// @return current style.
+//commandLineParser::parameterStyle commandLineParser::style(void) const
+//{
+//    return mStyle;
+//}
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // _COMMANDLINEPARSER_INL_H_
